cf4938e5e1c281a61f92109a2f39c24eb0c26139
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-09-14 16:31:26 -0400

    Update time_warp.py

--- SpecAugment/time_warp.py
+++ SpecAugment/time_warp.py
@@ -1,121 +1,182 @@
 import numpy as np
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
 from tensorflow_addons.image import sparse_image_warp
 
-x,sr = librosa.load(path = 'test.wav',
-                    sr = None,
-                    mono = True)
+def time_warp(mel_spec,W=30,show_result=False):
+    
+    """
+    Given a Mel-scale spectrogram with shape mel_spec_height x tau, time
+    warping is done as follows:
+    
+    1. Pick a column on the time axis at random from column number W to column
+       number tau - W, where W is the time warp parameter that is chosen
+       beforehand. In the original paper, W ranges from 0 to 80. Let the chosen
+       column number be alpha. This means that the Mel-spectrogram is now
+       partitioned into two windows defined on the time axis as:
+           
+       1st window:
+           column 0 --> column alpha
+           
+       2nd window:
+           column alpha + 1 --> column tau
+    
+    2. The 1st window is to be warped to the left or right by a distance w that
+       is sampled from a discrete uniform distribution with a support of [-W,W].
+       The 2nd window is warped in the opposite direction by the same amount
+       as the 1st window.
+    3. To perform this warping, control points are placed on each pixel in the
+       alpha column. The final locations of these pixels after warping are also
+       specified. Finally, the Tensorflow Addons function
+       tensorflow_addons.image.sparse_image_warp() is then used to perform time
+       warping.
+    
+    The original SpecAugment paper can be found here:
+        
+    https://arxiv.org/abs/1904.08779
+        
+    """
+    
+    # width and height of Mel-scale spectrogram. Height is equivalent to number
+    # of Mels and width is tau to match notation used in paper
+    
+    mel_spec_height, tau = mel_spec.shape
+    
+    # alpha is the column to be warped
+    
+    alpha = np.random.randint(W,tau - W)
 
-mel_spec = librosa.feature.melspectrogram(y = x,
-                                          sr = sr,
-                                          n_fft = 2048,
-                                          hop_length = 32,
-                                          power = 2.0,
-                                          n_mels = 256)
+    # each row of this array will contain the locations of the points to be
+    # warped. More precisely, it contains the coordinates of the alpha column,
+    # such that each pixel in the alpha column has its own coordinate
 
-# height is length of frequency axis, and width is length of time axis.
-# tau is the same notation used in the original paper.
+    src_ctrl_pt_loc = np.zeros((mel_spec_height,2),
+                               dtype=np.float32)
+    
+    # each row of the alpha column
+    
+    src_ctrl_pt_loc[:,0] = np.arange(mel_spec_height)
+    
+    # the alpha column
+    
+    src_ctrl_pt_loc[:,1] = alpha
 
-mel_spec_height, tau = mel_spec.shape
+    # add a batch dimension for the sparse_image_warp function to be used later
 
-"""
-Given a Mel-scale spectrogram with shape mel_spec_height x tau, time warping is
-done as follows:
+    src_ctrl_pt_loc = np.expand_dims(src_ctrl_pt_loc, 0)
+    
+    # sample w from a discrete uniform distribution
+    
+    w = np.random.randint(-W,W)
 
-1. Pick a column on the time axis at random from column number W to column
-   number tau - W, where W is the time warp parameter that is chosen beforehand.
-   In the original paper, W ranges from 0 to 80. Let the chosen column number
-   be alpha. This means that the Mel-spectrogram is now partitioned into two
-   windows defined by the coordinates:
-       
-   1st window:
-       row 0, column 0
-       row mel_spec_height, column 0
-       row 0, column alpha
-       row mel_spec_height, column alpha
-       
-   2nd window:
-       row 0, column alpha+1
-       row mel_spec_height, column alpha+1
-       row 0, column tau
-       row mel_spec_height, column tau
+    # each row of this array will contain the locations of the destinations that
+    # the points in src_ctrl_pt_loc will be warped to. These coordinates
+    # correspond to the target column
 
-2. The 1st window is to be warped to the left or right by a distance w that is
-   sampled from a discrete uniform distribution with a support of [-W,W].
-3. According to the original paper, to perform this warping, 6 control points
-   are placed on the edges of the window. 4 of these control points will be
-   placed on the corners of the window, and the other 2 will be placed at the
-   mid-points of the vertical edges of the window. However, the implementation
-   used here uses a different set of control points. This does not affect the
-   outcome.
-5. The Tensorflow Addons function tensorflow_addons.image.sparse_image_warp()
-   is then used to perform time warping.
-"""
+    dst_ctrl_pt_loc = np.zeros((mel_spec_height,2),
+                               dtype=np.float32)
+    
+    # each row of the target column
+    
+    dst_ctrl_pt_loc[:,0] = np.arange(mel_spec_height)
+    
+    # the target column
+    
+    dst_ctrl_pt_loc[:,1] = alpha + w
 
-# define the time warp parameter W
+    # add a batch dimension for the sparse_image_warp function to be used later
 
-W = 80
+    dst_ctrl_pt_loc = np.expand_dims(dst_ctrl_pt_loc, 0)
 
-# sample alpha. Note that this does not need to be a uniform distribution
+    # add batch and channel dimensions for the sparse_image_warp function to be
+    # used later
 
-alpha = np.random.randint(W,tau - W)
+    mel_spec = np.reshape(mel_spec,(1,*mel_spec.shape,1))
+    
+    """    
+    the sparse_image_warp function returns a tuple, where the first element
+    is the warped Mel-scale spectrogram and the second element is the dense
+    flow field that is produced by interpolation. Only the warped Mel-scale
+    spectrogram is of interest. Also, because the function returns a
+    tensorflow tensor, it is converted to a numpy array using the .numpy()
+    method.
+    
+    the num_boundary_points argument is set to be greater than 0 such that
+    frequency content is not lost while warping by constraining the warping.
+    """
+    
+    warped_spec = sparse_image_warp(
+                        image = mel_spec,
+                        source_control_point_locations = src_ctrl_pt_loc,
+                        dest_control_point_locations = dst_ctrl_pt_loc,
+                        interpolation_order = 2,
+                        regularization_weight = 0.0,
+                        num_boundary_points = 1)[0].numpy()
+    
+    # show original spectrogram and warped spectrogram
+    
+    if show_result:
+        
+        # original spectrogram
+        
+        plt.subplot(2,1,1)
+        
+        # convert to dB
+        
+        mel_spec = librosa.power_to_db(mel_spec[0,:,:,0],
+                                       ref=np.max)
+        
+        # plot vertical line showing the column that is warped
+        
+        mel_spec[:,alpha] = 0
+        
+        # display the original spectrogram
+        
+        librosa.display.specshow(mel_spec,
+                                 x_axis='time',
+                                 y_axis='mel')
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Mel spectrogram')
+        
+        # warped spectrogram
+        
+        plt.subplot(2,1,2)
+        
+        # convert to dB
+        
+        warped_spec2 = librosa.power_to_db(warped_spec[0,:,:,0],ref=np.max)
+        
+        # plot vertical line to show warping
+        
+        warped_spec2[:,alpha + w] = 0
+        
+        # display the warped spectrogram
+        
+        librosa.display.specshow(warped_spec2,
+                                 x_axis='time',
+                                 y_axis='mel')
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Mel spectrogram')
+        
+        plt.tight_layout()
+    
+    return warped_spec
 
-# the control points only need to be defined along the alpha column that
-# separates the two windows
+if __name__ == '__main__':
+    
+    import librosa
+    import librosa.display
+    import matplotlib.pyplot as plt
+    
+    x,sr = librosa.load(path = 'test.wav',
+                        sr = None,
+                        mono = True)
 
-# defining the vertical coordinates for the control points along the alpha
-# column
-
-src_ctr_pt_freq = np.arange(mel_spec_height // 2)
-
-# defining the horizontrol coordinates for the control points along the alpha
-# column
-
-src_ctr_pt_time = np.ones_like(src_ctr_pt_freq) * alpha
-
-# form the control points
-
-src_ctr_pts = np.stack((src_ctr_pt_freq,src_ctr_pt_time),
-                       axis = -1).astype(np.float32)
-
-# src_ctr_pts = tf.cast(src_ctr_pts, dtype=tf.float32)
-
-# sample w
-
-w = np.random.randint(-W,W)
-dest_ctr_pt_freq = src_ctr_pt_freq
-dest_ctr_pt_time = src_ctr_pt_time + w
-dest_ctr_pts = np.stack((dest_ctr_pt_freq, dest_ctr_pt_time),
-                        axis=-1).astype(np.float32)
-# dest_ctr_pts = tf.cast(dest_ctr_pts, dtype=tf.float32)
-
-# warp
-source_control_point_locations = np.expand_dims(src_ctr_pts, 0)  # (1, v//2, 2)
-dest_control_point_locations = np.expand_dims(dest_ctr_pts, 0)  # (1, v//2, 2)
-mel_spec = np.reshape(mel_spec,(1,*mel_spec.shape,1))
-
-warped_image, _ = sparse_image_warp(mel_spec,
-                                    source_control_point_locations,
-                                    dest_control_point_locations)
-
-warped_image = warped_image.numpy()
-
-image = warped_image[0,:,:,0]
-
-# plt.figure(figsize=(10, 4))
-ax1 = plt.subplot(2,1,1)
-librosa.display.specshow(librosa.power_to_db(mel_spec[0,:,:,0],ref=np.max),
-                          y_axis='mel',
-                          x_axis='time')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram')
-plt.vlines(x=alpha,ymin=0,ymax=mel_spec.shape[1])
-ax2 = plt.subplot(2,1,2)
-librosa.display.specshow(librosa.power_to_db(image,ref=np.max),
-                          y_axis='mel',
-                          x_axis='time')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram')
-plt.tight_layout()
+    mel_spec = librosa.feature.melspectrogram(y = x,
+                                              sr = sr,
+                                              n_fft = 2048,
+                                              hop_length = 32,
+                                              power = 2.0,
+                                              n_mels = 256)
+    
+    warped_spec = time_warp(mel_spec,
+                            W = 20,
+                            show_result = True)
