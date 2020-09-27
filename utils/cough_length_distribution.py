3bd420064390e040d1f99f572986f48b2d7c8a5c
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-09-27 16:17:35 -0400

    Update cough_length_distribution.py

--- utils/cough_length_distribution.py
+++ utils/cough_length_distribution.py
@@ -1,9 +1,21 @@
 import librosa
 import os
+import numpy as np
 
 data_dir = '../../data/cough'
 
-for file in os.listdir(data_dir):
+files = os.listdir(data_dir)
+
+lengths = np.zeros((len(files),))
+
+for i,file in enumerate(files):
     x,sr = librosa.load(path = os.path.join(data_dir,file),
                         sr = None)
-    
+    if len(x.shape) == 1: # if mono
+        lengths[i] = x.shape[0]/sr
+    else: # if stereo
+        lengths[i] = x.shape[1]/sr
+
+mean = lengths.mean()
+
+variance = lengths.var(ddof=1)
