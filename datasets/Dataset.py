871cbb2892b638f22124196ca093ae5220a651a0
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-09-29 16:01:10 -0400

    Update Dataset.py

--- datasets/Dataset.py
+++ datasets/Dataset.py
@@ -12,8 +12,6 @@ import torchaudio
 
 import torchvision as tv
 
-from sklearn.model_selection import train_test_split
-
 import scipy
 
 import numpy as np
@@ -67,15 +65,15 @@ class Dataset(torch.utils.data.Dataset):
         self._get_cough_length_stats()
         
         """
-        compute the mean root mean square and entropy values for each frame of
-        cough signals. These provide thresholds to be used to control the
-        admission of audio that is cropped from speech and other files. More
-        precisely, if a majority of frames satisfy these thresholds, then
-        the audio snippet is admitted. Otherwise, it is discarded.
+        compute the average root mean square (rms) and entropy values for
+        each frame of cough signals. These provide thresholds to be used
+        to control the admission of audio that is cropped from speech and
+        other files. More precisely, if a majority of frames satisfy these
+        thresholds, then the audio snippet is admitted. Otherwise, it is
+        discarded.
         """
         
         self._get_cough_frame_stats()
-        
         
     def _get_file_paths(self):
         
@@ -93,39 +91,76 @@ class Dataset(torch.utils.data.Dataset):
         """
         depending on which network will be trained, validated, or tested,
         assign the corresponding paths and labels. If the reconstruction
-        network is of interest, then 
+        network is of interest, then only the speech signals are provided.
+        If the discrimination network is of interest, then all signals are
+        provided. If both networks are of interest, then all signals are
+        provided. Note that the reconstruction network does not need labels
         """
         
-        if net_type == 'recon':
+        if self.net_type == 'recon':
             self.paths = self.splits['speech_'+self.mode]
         else:
             self.paths = (self.splits['other_'+self.mode] + 
                           self.splits['cough_'+self.mode] + 
                           self.splits['speech_'+self.mode])
-            self.labels = ([0]*len(self.splits['other_'+self.mode] +
-                           [1]*len(self.splits['cough_'+self.mode] +
-                           [2]*len(self.splits['speech_'+self.mode])
-        
+            self.labels = ([0]*len(self.splits['other_'+self.mode]) +
+                           [1]*len(self.splits['cough_'+self.mode]) +
+                           [2]*len(self.splits['speech_'+self.mode]))
+    
     def _get_cough_length_stats(self):
-
+        
+        # list of cough files
+        
         files = self.splits['cough_'+self.mode]
+        
+        # to store the length of each cough
         
         lengths = np.zeros((len(files),))
         
+        # record the length of each cough
+        
         for i,file in enumerate(files):
             lengths[i] = librosa.get_duration(filename = file)
+        
+        # compute the mean and variance of the lengths of each cough.
+        # These are unbiased maximum-likelihood estimates
         
         self.cough_length_mean = lengths.mean()
         self.cough_length_var = lengths.var(ddof=1)
     
     def _get_cough_frame_stats(self):
         
+        """
+        for each cough signal, split it into 64 ms frames. Then, for each
+        frame, compute the root mean square and entropy values. Entropy
+        values are computed by normalizing the discrete Fourier transform
+        of the samples in a frame and the result is treated as a
+        probability distribution.
+        
+        See section 4.1 in the following paper for details:
+            
+        "SoundSense: Scalable Sound Sensing for People-Centric
+        Applications on Mobile Phones"
+        
+        Available here:
+            
+        https://pac.cs.cornell.edu/pubs/mobisys09_soundsense.pdf
+        
+        """
+        
+        # list of cough files
+        
         files = self.splits['cough_'+self.mode]
         
-        # rms in first column, entropy in second column
-        # stats = np.zeros((len(files),2))
+        # to store the average rms and entropy values for each cough
+        # signal. Average rms values are saved in the first column while
+        # average entropy values are saved in the second column
+        
+        stats = np.zeros((len(files),2))     
         
         for i,file in enumerate(files):
+            
+            # load cough signal
             
             x,sr = librosa.load(path = file,sr = None,mono = True)
             
@@ -133,50 +168,254 @@ class Dataset(torch.utils.data.Dataset):
             
             frame_width = int(0.064 * sr)
             
+            # compute number of frames in cough signal
+            
             num_frames = int(np.floor(x.shape[0]/frame_width))
             
-            # note that for a numpy array b, b[x:infinity] is the same
-            # as b[x:]
+            # to store rms and entropy values for each frame in a cough
+            # signal
             
-            rms_val = []
-            entropies = []
+            frames_rms = []
+            frames_entropy = []
             
-            # frames with length shorter than frame_width are given same weight as
-            # frames with length of frame_width, which will lead to a biased rms value
+            """
+            for each frame in the cough signal, compute the rms value
+            of that frame and the entropy of that frame. These values
+            are then appended to the rms_frames and entropy_frames above.
+            
+            Note that since the average of these frame values will
+            eventually be computed, then frames with a length shorter than
+            frame_width would be given the same weight as frames with a
+            length of frame_width, which would lead to biased estimates
+            of the average of the rms and entropy values of these frames.
+            Therefore, frames shorter than frame_width are ignored.
+            
+            For example, suppose that a frame has a length of 4500 samples,
+            and frame_width is 1024 samples. This means that there are
+            4500/1024 = 4.395... frames. So, the first 4 frames of lengths
+            1024 samples will be considered. The final frame with a length
+            of 4500 - (1024*4) = 404 samples will not be considered.
+            """
             
             for j in range(num_frames):
+                
+                # extract a frame
+                
                 frame = x[j*frame_width:(j+1)*frame_width]
+                
+                # compute rms and save the value
+                
                 rms = np.sqrt(np.mean(frame**2))
-                rms_val.append(rms)
+                frames_rms.append(rms)
+                
+                # apply a hanning window to mitigate spectral leakage
+                
                 window = scipy.signal.windows.hann(frame.shape[0])
                 y = np.multiply(frame,window)
-                Y = np.abs(scipy.fft.fft(y))
-                Y = Y/Y.sum() # make probability dist
-                entropy = -np.sum(np.multiply(Y,np.log(Y)))
-                entropies.append(entropy)
                 
-            rms_mean = sum(rms_val)/len(rms_val)
-            entropy_mean = sum(entropies)/len(entropies)
+                # compute magnitude of discrete Fourier transform
+                
+                Y = np.abs(scipy.fft.fft(y))
+                
+                # normalize to make it a probability distribution
+                
+                Y = Y/Y.sum()
+                
+                # compute entropy and save the value
+                
+                entropy = -np.sum(np.multiply(Y,np.log(Y)))
+                frames_entropy.append(entropy)
             
-            stats[i,0] = rms_mean
-            stats[i,1] = entropy_mean
+            # compute the average rms for all frames (except the last one)
+            # in the cough signal
             
-        print(stats.mean(axis=0))
+            frames_rms_mean = sum(frames_rms)/len(frames_rms)
+            frames_entropy_mean = sum(frames_entropy)/len(frames_entropy)
+            
+            # save these average values
+            
+            stats[i,0] = frames_rms_mean
+            stats[i,1] = frames_entropy_mean
+            
+        self.rms_threshold = stats[:,0].mean()
+        self.entropy_threshold = stats[:,1].mean()
     
     def __len__(self):
         return len(self.paths)
     
+    def _sample_cough_length(self):
+        
+        x = self.rng.normal(self.cough_length_mean,
+                            np.sqrt(self.cough_length_var),
+                            1)
+        
+        # cough length can't be negative or greater than 1. If it is,
+        # sample from uniform distribution.
+        
+        if x < 0 or x >= 1:
+            
+            x = self.rng.uniform(0.0,1.0,1)
+            
+        return x
+    
+    def _check_audio_snippet(self,audio,frame_length=0.064,
+                             majority_ratio=0.8):
+        
+        """
+        Split short (< 1 second) audio snippets into frames of length frame_length.
+        Then, for each frame, compute the root mean square (rms) and
+        entropy values. Entropy values are computed by normalizing the
+        discrete Fourier transform of the samples in a frame and the result
+        is treated as a probability distribution. Next, if majority_ratio
+        of frames achieve rms values above the threshold of:
+            
+        self.rms_threshold
+        
+        And achieve entropy values below the threshold of:
+            
+        self.entropy_threshold
+        
+        See section 4.1 in the following paper for details:
+            
+        "SoundSense: Scalable Sound Sensing for People-Centric
+        Applications on Mobile Phones"
+        
+        Available here:
+            
+        https://pac.cs.cornell.edu/pubs/mobisys09_soundsense.pdf
+        
+        """
+        
+        # list of cough files
+        
+        files = self.splits['cough_'+self.mode]
+        
+        # to store the average rms and entropy values for each cough
+        # signal. Average rms values are saved in the first column while
+        # average entropy values are saved in the second column
+        
+        stats = np.zeros((len(files),2))     
+        
+        for i,file in enumerate(files):
+            
+            # load cough signal
+            
+            x,sr = librosa.load(path = file,sr = None,mono = True)
+            
+            # frames are 64 ms wide
+            
+            frame_width = int(0.064 * sr)
+            
+            # compute number of frames in cough signal
+            
+            num_frames = int(np.floor(x.shape[0]/frame_width))
+            
+            # to store rms and entropy values for each frame in a cough
+            # signal
+            
+            frames_rms = []
+            frames_entropy = []
+            
+            """
+            for each frame in the cough signal, compute the rms value
+            of that frame and the entropy of that frame. These values
+            are then appended to the rms_frames and entropy_frames above.
+            
+            Note that since the average of these frame values will
+            eventually be computed, then frames with a length shorter than
+            frame_width would be given the same weight as frames with a
+            length of frame_width, which would lead to biased estimates
+            of the average of the rms and entropy values of these frames.
+            Therefore, frames shorter than frame_width are ignored.
+            
+            For example, suppose that a frame has a length of 4500 samples,
+            and frame_width is 1024 samples. This means that there are
+            4500/1024 = 4.395... frames. So, the first 4 frames of lengths
+            1024 samples will be considered. The final frame with a length
+            of 4500 - (1024*4) = 404 samples will not be considered.
+            """
+            
+            for j in range(num_frames):
+                
+                # extract a frame
+                
+                frame = x[j*frame_width:(j+1)*frame_width]
+                
+                # compute rms and save the value
+                
+                rms = np.sqrt(np.mean(frame**2))
+                frames_rms.append(rms)
+                
+                # apply a hanning window to mitigate spectral leakage
+                
+                window = scipy.signal.windows.hann(frame.shape[0])
+                y = np.multiply(frame,window)
+                
+                # compute magnitude of discrete Fourier transform
+                
+                Y = np.abs(scipy.fft.fft(y))
+                
+                # normalize to make it a probability distribution
+                
+                Y = Y/Y.sum()
+                
+                # compute entropy and save the value
+                
+                entropy = -np.sum(np.multiply(Y,np.log(Y)))
+                frames_entropy.append(entropy)
+            
+            # compute the average rms for all frames (except the last one)
+            # in the cough signal
+            
+            frames_rms_mean = sum(frames_rms)/len(frames_rms)
+            frames_entropy_mean = sum(frames_entropy)/len(frames_entropy)
+            
+            # save these average values
+            
+            stats[i,0] = frames_rms_mean
+            stats[i,1] = frames_entropy_mean
+            
+        self.rms_threshold = stats[:,0].mean()
+        self.entropy_threshold = stats[:,1].mean()
+        
     def __getitem__(self,idx):
         
-        # assign labels for cough and non-cough files
+        # if reconstruction network is of interest, speech audio only
+        # is available
         
-        label = self.labels[idx]
+        if self.net_type == 'recon':
+            label = 2
+        else:
+            label = self.labels[idx]
         
-        # load waveform
-                
-        waveform,_ = librosa.load(path = self.paths[idx],
-                                  sr = self.sample_rate,
-                                  mono = True)
+        """
+        if not cough signal, then sample snippet of audio with length that
+        is sampled from normal distribution and location that is sampled
+        from uniform distribution.
+        """
+        
+        if label != 1:
+        
+            sample_length_sec = self._sample_cough_length()
+            file_length_sec = librosa.get_duration(
+                                    filename = self.paths[idx])
+            offset = self.rng.uniform(0.0,
+                                      file_length_sec - sample_length_sec,
+                                      1)
+            x,_ = librosa.load(path = self.paths[idx],
+                               sr = self.sample_rate,
+                               mono = True,
+                               offset = offset,
+                               duration = sample_length_sec)
+            
+        else: # if cough
+            
+            x,_ = librosa.load(path = self.paths[idx],
+                               sr = self.sample_rate,
+                               mono = True)
+            
+        
+        
         
         # if the audio file is shorter than 9 seconds, pad it with zeros to be
         # 10 seconds long, and if it is longer than 11 seconds, decimate it to
@@ -249,7 +488,7 @@ class Dataset(torch.utils.data.Dataset):
         log_mel_spec = normalize_func(log_mel_spec)
         
         return (log_mel_spec,label)
-
+            
 if __name__ == '__main__':
 
     import matplotlib.pyplot as plt
