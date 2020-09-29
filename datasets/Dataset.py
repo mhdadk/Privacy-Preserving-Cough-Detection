08fb31bdd7ee837d094a7bac9910bce8a4975fcc
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-09-29 12:05:20 -0400

    Update Dataset.py

--- datasets/Dataset.py
+++ datasets/Dataset.py
@@ -18,89 +18,159 @@ import scipy
 
 import numpy as np
 
-import csv
+import pickle
 
 class Dataset(torch.utils.data.Dataset):
     
-    def __init__(self,data_dir,data_split_dir,sample_rate,mode):
+    def __init__(self,data_split_dir,sample_rate,net_type,mode):
         
-        # what to resample audio to
+        """
+        training 'train', validation 'val', or testing 'test' mode
+        """
+        
+        self.mode = mode
+        
+        """
+        Where the pickled lists containing paths to the training, validation,
+        and testing files are stored
+        """
+        
+        self.data_split_dir = data_split_dir
+        
+        # what sampling rate to resample audio to
         
         self.sample_rate = sample_rate
         
-        # get training, validation, and testing splits
+        """
+        If this data is for the discrimination 'disc' network,
+        reconstruction 'recon' network, or 'both'
+        """
+        
+        self.net_type = net_type
+        
+        """
+        get paths to the training, validation, and testing files
+        """
+        
+        self._get_file_paths()
+        
+        # initialize random number generator
+        
+        self.rng = np.random.default_rng()
+        
+        """
+        compute the mean and variance of the lengths of coughs. These will be
+        used to sample lengths of audio to crop out of speech and other audio
+        files
+        """
+        
+        self._get_cough_length_stats()
+        
+        """
+        compute the mean root mean square and entropy values for each frame of
+        cough signals. These provide thresholds to be used to control the
+        admission of audio that is cropped from speech and other files. More
+        precisely, if a majority of frames satisfy these thresholds, then
+        the audio snippet is admitted. Otherwise, it is discarded.
+        """
+        
+        self._get_cough_frame_stats()
         
         
+    def _get_file_paths(self):
         
-        with open('../../data_split/cough_train.csv') as file:
-            reader = csv.reader(file,delimiter='\n')
-            self.cough_train = [x[0] for x in reader]
-            
+        # to store each un-pickled list
         
+        self.splits = {}
         
-        # file names
+        # for each pickled file
         
-        self.coughs = os.listdir(os.path.join(data_dir,'cough'))
-        self.speech = os.listdir(os.path.join(data_dir,'speech'))
-        self.other = os.listdir(os.path.join(data_dir,'other'))
-        self.filenames = self.other + self.cough + self.speech
+        for file in os.listdir(self.data_split_dir):
+            with open(os.path.join(self.data_split_dir,file),'rb') as fp:
+                # file[:-4] is used to discard the 'pkl' substring at the end 
+                self.splits[file[:-4]] = pickle.load(fp)
+
+        """
+        depending on which network will be trained, validated, or tested,
+        assign the corresponding paths and labels. If the reconstruction
+        network is of interest, then 
+        """
         
-        # labels
-        
-        self.labels = ([0]*len(self.other) + [1]*len(self.cough) + 
-                       [2]*len(self.speech))
-        
-        # seed for random number generator
-        
-        rng_seed = 42
-        
-        # create a 70-30 training-testing split
-        
-        (self.paths_train,
-         self.paths_test,
-         self.labels_train,
-         self.labels_test) = train_test_split(self.paths_all,
-                                              self.labels_all,
-                                              train_size = 0.7,
-                                              test_size = 0.3,
-                                              random_state = rng_seed,
-                                              shuffle = True,
-                                              stratify = self.labels_all)
-        
-        # create a 70-15-15 training-validation-testing split
-                                              
-        (self.paths_val,
-         self.paths_test,
-         self.labels_val,
-         self.labels_test) = train_test_split(self.paths_test,
-                                              self.labels_test,
-                                              train_size = 0.5,
-                                              test_size = 0.5,
-                                              random_state = rng_seed,
-                                              shuffle = True,
-                                              stratify = self.labels_test)
-                                              
-        # pick the training, validation, or testing set based on the mode
-        
-        if mode == 'train':
-            self.paths = self.paths_train
-            self.labels = self.labels_train
-        elif mode == 'val':
-            self.paths = self.paths_val
-            self.labels = self.labels_val
+        if net_type == 'recon':
+            self.paths = self.splits['speech_'+self.mode]
         else:
-            self.paths = self.paths_test
-            self.labels = self.labels_test
+            self.paths = (self.splits['other_'+self.mode] + 
+                          self.splits['cough_'+self.mode] + 
+                          self.splits['speech_'+self.mode])
+            self.labels = ([0]*len(self.splits['other_'+self.mode] +
+                           [1]*len(self.splits['cough_'+self.mode] +
+                           [2]*len(self.splits['speech_'+self.mode])
+        
+    def _get_cough_length_stats(self):
+
+        files = self.splits['cough_'+self.mode]
+        
+        lengths = np.zeros((len(files),))
+        
+        for i,file in enumerate(files):
+            lengths[i] = librosa.get_duration(filename = file)
+        
+        self.cough_length_mean = lengths.mean()
+        self.cough_length_var = lengths.var(ddof=1)
+    
+    def _get_cough_frame_stats(self):
+        
+        files = self.splits['cough_'+self.mode]
+        
+        # rms in first column, entropy in second column
+        # stats = np.zeros((len(files),2))
+        
+        for i,file in enumerate(files):
+            
+            x,sr = librosa.load(path = file,sr = None,mono = True)
+            
+            # frames are 64 ms wide
+            
+            frame_width = int(0.064 * sr)
+            
+            num_frames = int(np.floor(x.shape[0]/frame_width))
+            
+            # note that for a numpy array b, b[x:infinity] is the same
+            # as b[x:]
+            
+            rms_val = []
+            entropies = []
+            
+            # frames with length shorter than frame_width are given same weight as
+            # frames with length of frame_width, which will lead to a biased rms value
+            
+            for j in range(num_frames):
+                frame = x[j*frame_width:(j+1)*frame_width]
+                rms = np.sqrt(np.mean(frame**2))
+                rms_val.append(rms)
+                window = scipy.signal.windows.hann(frame.shape[0])
+                y = np.multiply(frame,window)
+                Y = np.abs(scipy.fft.fft(y))
+                Y = Y/Y.sum() # make probability dist
+                entropy = -np.sum(np.multiply(Y,np.log(Y)))
+                entropies.append(entropy)
+                
+            rms_mean = sum(rms_val)/len(rms_val)
+            entropy_mean = sum(entropies)/len(entropies)
+            
+            stats[i,0] = rms_mean
+            stats[i,1] = entropy_mean
+            
+        print(stats.mean(axis=0))
     
     def __len__(self):
-        
         return len(self.paths)
     
     def __getitem__(self,idx):
         
         # assign labels for cough and non-cough files
         
-        label = torch.tensor([self.labels[idx]])
+        label = self.labels[idx]
         
         # load waveform
                 
