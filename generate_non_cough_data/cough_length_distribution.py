c467a2f2216cd451ecc205be81f6e9e35d8c19e4
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-10-02 11:23:47 -0400

    Update cough_length_distribution.py

--- generate_non_cough_data/cough_length_distribution.py
+++ generate_non_cough_data/cough_length_distribution.py
@@ -8,11 +8,16 @@ files = os.listdir(data_dir)
 
 lengths = np.zeros((len(files),))
 
+longer = []
+
 for i,file in enumerate(files):
     x,sr = librosa.load(path = os.path.join(data_dir,file),
                         sr = None,
                         mono = True)
     lengths[i] = x.shape[0]/sr
+    if lengths[i] > 1.0:
+        longer.append(file)
+        
 
 mean = lengths.mean()
 
