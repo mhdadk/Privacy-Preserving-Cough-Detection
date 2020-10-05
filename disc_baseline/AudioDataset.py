9c88c072ba858d4e522ec000a4bdef0de1158b3b
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-10-04 22:32:42 -0400

    Update AudioDataset.py

--- disc_baseline/AudioDataset.py
+++ disc_baseline/AudioDataset.py
@@ -5,13 +5,13 @@ import torchaudio
 
 class AudioDataset(torch.utils.data.Dataset):
     
-    def __init__(self,audio_type,data_split_dir,sample_rate,mode):
+    def __init__(self,net_type,data_split_dir,sample_rate,mode):
         
         """
-        whether to sample 'other', 'cough', or 'speech' data
+        whether to sample cough and other data, or only speech data
         """
         
-        self.audio_type = audio_type
+        self.net_type = net_type
         
         """
         Where the pickled lists containing paths to the training, validation,
@@ -45,10 +45,9 @@ class AudioDataset(torch.utils.data.Dataset):
         # for each pickled file
         
         for file in os.listdir(self.data_split_dir):
-            if self.audio_type in file:
-                with open(os.path.join(self.data_split_dir,file),'rb') as fp:
-                    # file[:-4] is used to discard the 'pkl' substring at the end 
-                    self.splits[file[:-4]] = pickle.load(fp)
+            with open(os.path.join(self.data_split_dir,file),'rb') as fp:
+                # file[:-4] is used to discard the 'pkl' substring at the end 
+                self.splits[file[:-4]] = pickle.load(fp)
                 
         """
         depending on which network will be trained, validated, or tested,
@@ -59,13 +58,12 @@ class AudioDataset(torch.utils.data.Dataset):
         provided. Note that the reconstruction network does not need labels
         """
         
-        if self.audio_type == 'other':
-            self.paths = self.splits['other_'+self.mode]
-            self.labels = [0]*len(self.splits['other_'+self.mode])
-        elif self.audio_type == 'cough':
-            self.paths = self.splits['cough_'+self.mode]
-            self.labels = [1]*len(self.splits['cough_'+self.mode])
-        else:
+        if self.net_type == 'disc': # discriminator network
+            self.paths = (self.splits['other_'+self.mode] + 
+                          self.splits['cough_'+self.mode])
+            self.labels = ([0]*len(self.splits['other_'+self.mode]) + 
+                           [1]*len(self.splits['cough_'+self.mode]))
+        else: # reconstruction network
             self.paths = self.splits['speech_'+self.mode]
             self.labels = [0]*len(self.splits['speech_'+self.mode])
     
@@ -94,14 +92,13 @@ class AudioDataset(torch.utils.data.Dataset):
         
         x = torchaudio.transforms.Resample(sr,self.sample_rate)(x)
         
-        # pad cough signals with zeros to make them 1 second long
+        # pad signals with zeros to make sure they are 1 second long
         
-        if label == 1:
-            pad_length = self.sample_rate - x.shape[0]
-            x = torch.nn.functional.pad(input = x,
-                                        pad = (0,pad_length),
-                                        mode = 'constant',
-                                        value = 0)
+        pad_length = self.sample_rate - x.shape[0]
+        x = torch.nn.functional.pad(input = x,
+                                    pad = (0,pad_length),
+                                    mode = 'constant',
+                                    value = 0)
         
         return (x,label)
             
@@ -111,11 +108,11 @@ if __name__ == '__main__':
     import sounddevice as sd
     import random
     
-    audio_type = 'speech'
+    net_type = 'recon'
     data_split_dir = '../../data_split'
     sample_rate = 16000
-    mode = 'train'
-    dataset = AudioDataset(audio_type,data_split_dir,sample_rate,mode)
+    mode = 'val'
+    dataset = AudioDataset(net_type,data_split_dir,sample_rate,mode)
     
     idx = random.randint(0,len(dataset))
     x,label = dataset[idx]
