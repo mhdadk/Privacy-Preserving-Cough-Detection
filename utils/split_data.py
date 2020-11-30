015eb88dc01966420c319881b00bb775133935d9
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-11-30 23:56:27 +0400

    Update split_data.py

--- utils/split_data.py
+++ utils/split_data.py
@@ -1,63 +1,50 @@
 import os
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import pickle
-from glob import glob
 
-data_dir = '../../temp_data/short'
+data_dir = '../../datasets/2'
 data_split_dir = '../../data_split'
 
 paths = []
 
-for i,label1 in enumerate(os.listdir(data_dir)):
-    for j,label2 in enumerate(os.listdir(os.path.join(data_dir,label1))):
-        for file in os.listdir(os.path.join(data_dir,label1,label2)):
-            paths.append([os.path.join(label1,file),i])
-    
+for i,label in enumerate(os.listdir(data_dir)):
+    for file in os.listdir(os.path.join(data_dir,label)):
+        paths.append([os.path.join(label,file),i])
+
 data = pd.DataFrame(paths)
 
 rng_seed = 42
 
-X_train,X_val,y_train,y_val = train_test_split(data.iloc[:,0], # paths
-                                               data.iloc[:,1], # labels
-                                               train_size = 0.8,
-                                               random_state = rng_seed,
-                                               shuffle = True,
-                                               stratify = data.iloc[:,1])
+data_train,data_val = train_test_split(data,
+                                       train_size = 0.8,
+                                       random_state = rng_seed,
+                                       shuffle = True,
+                                       stratify = data.iloc[:,1])
 
+data_val,data_test = train_test_split(data_val,
+                                      train_size = 0.5,
+                                      random_state = rng_seed,
+                                      shuffle = True,
+                                      stratify = data_val.iloc[:,1])
 
+# show training, validation, and testing class ratios
 
+for data,name in zip([data_train,data_val,data_test],
+                     ['train','val','test']):
+    for label in os.listdir(data_dir):
+        print('{} {} ratio = {}'.format(label,name,
+                                        sum(data.iloc[:,1] == i)/len(data)))
+    
+# write to csv
 
+data_train.to_csv(os.path.join(data_split_dir,'data_train.csv'),
+                  header = False,
+                  index = False)
 
-    # filenames = glob(os.path.join(data_dir,label,'*'))
-    
-    # # for i in range(len(filenames)):
-    # #     filenames[i] = filenames[i].replace('\\','/')
-    
-    # rng_seed = 42
-    
-    # train,val = train_test_split(filenames,
-    #                              train_size = 0.8,
-    #                              random_state = rng_seed,
-    #                              shuffle = True,
-    #                              stratify = None)
+data_val.to_csv(os.path.join(data_split_dir,'data_val.csv'),
+                header = False,
+                index = False)
 
-    # val,test = train_test_split(val,
-    #                             train_size = 0.5,
-    #                             random_state = rng_seed,
-    #                             shuffle = True,
-    #                             stratify = None)
-    
-    # print('{} train ratio = {}'.format(label,len(train)/len(filenames)))
-    # print('{} val ratio = {}'.format(label,len(val)/len(filenames)))
-    # print('{} test ratio = {}'.format(label,len(test)/len(filenames)))
-    
-    # with open(os.path.join(data_split_dir,label+'_train.pkl'),'wb') as fp:
-    #     pickle.dump(train,fp)
-    
-    # with open(os.path.join(data_split_dir,label+'_val.pkl'),'wb') as fp:
-    #     pickle.dump(val,fp)
-    
-    # with open(os.path.join(data_split_dir,label+'_test.pkl'),'wb') as fp:
-    #     pickle.dump(test,fp)
-    
+data_test.to_csv(os.path.join(data_split_dir,'data_test.csv'),
+                 header = False,
+                 index = False)
