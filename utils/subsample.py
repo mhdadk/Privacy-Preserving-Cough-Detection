465c3193c026738b1522b04a9a1343d6119bafdb
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-12-06 13:15:44 +0400

    rename file

--- utils/subsample.py
+++ utils/subsample.py
@@ -4,12 +4,13 @@ import shutil
 
 src_dir = '../../datasets/1'
 dst_dir = '../../datasets/8'
+num_cough_files = 431
 
 for label in os.listdir(src_dir):
     if label == 'COUGH':
         continue
     files = os.listdir(os.path.join(src_dir,label))
-    subsample = random.sample(files,431)
+    subsample = random.sample(files,num_cough_files)
     for file in subsample:
         shutil.copy2(os.path.join(src_dir,label,file),
                      os.path.join(dst_dir,label,file))
