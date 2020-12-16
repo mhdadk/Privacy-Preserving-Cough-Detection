bdbc79c20d622f54fb3e15aa7233ffa3ed33258e
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-12-16 18:16:47 +0400

    Update recon.py

--- unit_tests/recon.py
+++ unit_tests/recon.py
@@ -1,66 +1,95 @@
 import torch
-
-x = torch.randn(32,1,3000)
-
-# initial filter
-
-filt = torch.nn.Conv1d(in_channels = 1,
-                       out_channels = 1,
-                       kernel_size = 40,
-                       stride = 1)
-
-y1 = filt(x)
+import functools, operator
 
 # reconstruction network
 
+batch_size = 8
+num_channels = 1
+length_sec = 1.5
+sample_rate = 2000
+
+# to count the total number of parameters
+
+op_params = []
+
+# example input
+
+x = torch.randn(batch_size,num_channels,int(length_sec * sample_rate))
+
+"""
+scale each signal to be between 0 and 1. This is equivalent to:
+
+y = {x / (x.max - x.min)} - {x.min / (x.max - x.min)}
+  = (1 / x.max - x.min) * (x - x.min)
+
+"""
+
+x = torch.multiply(torch.div(1,x.max(dim = 2)[0].unsqueeze(dim = -1) - 
+                               x.min(dim = 2)[0].unsqueeze(dim = -1)),
+                   x - x.min(dim = 2)[0].unsqueeze(dim = -1))
+
 # encode
+
+activation = torch.nn.SELU()
+
+conv1 = torch.nn.Conv1d(in_channels = 1,
+                        out_channels = 32,
+                        kernel_size = 32,
+                        stride = 1)
+
+op_params.append(conv1)
+
+y1 = activation(conv1(x))
 
 pool1 = torch.nn.MaxPool1d(kernel_size = 2)
 
 y2 = pool1(y1)
 
-conv1 = torch.nn.Conv1d(in_channels = 1,
-                        out_channels = 1,
-                        kernel_size = 40,
+conv2 = torch.nn.Conv1d(in_channels = 32,
+                        out_channels = 56,
+                        kernel_size = 16,
                         stride = 1)
 
-y3 = conv1(y2)
+op_params.append(conv2)
+
+y3 = activation(conv2(y2))
 
 y4 = pool1(y3)
 
-conv2 = torch.nn.Conv1d(in_channels = 1,
-                        out_channels = 1,
-                        kernel_size = 40,
-                        stride = 1)
-
-y5 = conv2(y4)
-
-y6 = pool1(y5)
-
 # decode
 
-us1 = torch.nn.Upsample(scale_factor = 3,
+us1 = torch.nn.Upsample(scale_factor = 2,
                         mode = 'nearest')
 
-y7 = us1(y6)
+y5 = us1(y4)
 
-conv3 = torch.nn.Conv1d(in_channels = 1,
-                        out_channels = 1,
-                        kernel_size = 15,
-                        stride = 1,
-                        padding = 0)
+conv3 = torch.nn.Conv1d(in_channels = 56,
+                        out_channels = 32,
+                        kernel_size = 16,
+                        stride = 1)
 
-y8 = conv3(y7)
+op_params.append(conv3)
 
-us2 = torch.nn.Upsample(scale_factor = 3,
+y6 = activation(conv3(y5))
+
+us2 = torch.nn.Upsample(size = 3031,
                         mode = 'nearest')
 
-y9 = us2(y8)
+y7 = us2(y6)
 
-conv4 = torch.nn.Conv1d(in_channels = 1,
+conv4 = torch.nn.Conv1d(in_channels = 32,
                         out_channels = 1,
-                        kernel_size = 19,
-                        stride = 1,
-                        padding = 0)
+                        kernel_size = 32,
+                        stride = 1)
 
-y10 = conv4(y9)
+op_params.append(conv4)
+
+y8 = torch.sigmoid(conv4(y7))
+
+# compute total number of parameters
+
+num_params = 0
+
+for op in op_params:
+    num_params += functools.reduce(operator.mul,op.weight.shape) + \
+                  functools.reduce(operator.mul,op.bias.shape)
