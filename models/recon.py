262ea815679a0e7c0311c8eff5e2c6d4315927ab
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2020-12-21 16:10:12 +0400

    Update recon.py

--- models/recon.py
+++ models/recon.py
@@ -1,59 +1,124 @@
 import torch
 from collections import OrderedDict
 
+class PTanh(torch.nn.Module):
+    
+    # PTanh(x) = a * tanh(b * x)
+    
+    def __init__(self,num_parameters):
+        
+        super().__init__()
+        
+        # initialize a
+        
+        a = torch.full((num_parameters,1), 1.7159)
+        # torch.nn.init.normal_(a)
+        self.a = torch.nn.Parameter(a,requires_grad = True)
+        
+        # initialize b
+        
+        b = torch.full((num_parameters,1), 2/3)
+        # torch.nn.init.normal_(b)
+        self.b = torch.nn.Parameter(b,requires_grad = True)
+    
+    def forward(self,x):
+        x = torch.multiply(x,self.a)
+        x = torch.multiply(torch.nn.Tanh()(x),self.b)
+        return x
+
 class Autoencoder(torch.nn.Module):
     
     def __init__(self):
         
         super().__init__()
         
-        # encoder
+        # encoder --------------------------------------------------------
         
         encoder = OrderedDict()
         
+        # first layer
+        
         encoder['conv1'] = torch.nn.Conv1d(in_channels = 1,
+                                           out_channels = 16,
+                                           kernel_size = 3,
+                                           stride = 1)
+        
+        encoder['act1'] = PTanh(16)
+        
+        encoder['out1'] = torch.nn.Conv1d(in_channels = 16,
+                                          out_channels = 16,
+                                          kernel_size = 2,
+                                          stride = 2)
+        
+        # second layer
+        
+        encoder['conv2'] = torch.nn.Conv1d(in_channels = 16,
                                            out_channels = 32,
-                                           kernel_size = 32,
+                                           kernel_size = 5,#3,
                                            stride = 1)
         
-        encoder['act1'] = torch.nn.SELU()
+        encoder['act2'] = PTanh(32)
         
-        encoder['out1'] = torch.nn.MaxPool1d(kernel_size = 2)
+        encoder['out2'] = torch.nn.Conv1d(in_channels = 32,
+                                          out_channels = 32,
+                                          kernel_size = 2,
+                                          stride = 2)
         
-        encoder['conv2'] = torch.nn.Conv1d(in_channels = 32,
-                                           out_channels = 56,
-                                           kernel_size = 16,
+        # third layer
+        
+        encoder['conv3'] = torch.nn.Conv1d(in_channels = 32,
+                                           out_channels = 64,
+                                           kernel_size = 7,#3,
                                            stride = 1)
         
-        encoder['act2'] = torch.nn.SELU()
+        encoder['act3'] = PTanh(64)
         
-        encoder['out2'] = torch.nn.MaxPool1d(kernel_size = 2)
+        encoder['out3'] = torch.nn.Conv1d(in_channels = 64,
+                                          out_channels = 64,
+                                          kernel_size = 2,
+                                          stride = 2)
         
         self.encoder = torch.nn.Sequential(encoder)
         
-        # decoder
+        # decoder --------------------------------------------------------
         
         decoder = OrderedDict()
         
-        decoder['us1'] = torch.nn.Upsample(scale_factor = 2,
+        # fourth layer
+        
+        decoder['us4'] = torch.nn.Upsample(scale_factor = 2,
                                            mode = 'nearest')
         
-        decoder['conv3'] = torch.nn.Conv1d(in_channels = 56,
+        decoder['conv4'] = torch.nn.Conv1d(in_channels = 64,
                                            out_channels = 32,
-                                           kernel_size = 16,
+                                           kernel_size = 7,#3,
                                            stride = 1)
         
-        decoder['out3'] = torch.nn.SELU()
+        decoder['out4'] = PTanh(32)
         
-        decoder['us2'] = torch.nn.Upsample(size = 3031,
-                                      mode = 'nearest')
+        # fifth layer
         
-        decoder['conv4'] = torch.nn.Conv1d(in_channels = 32,
-                                           out_channels = 1,
-                                           kernel_size = 32,
+        decoder['us5'] = torch.nn.Upsample(scale_factor = 2,
+                                           mode = 'nearest')
+        
+        decoder['conv5'] = torch.nn.Conv1d(in_channels = 32,
+                                           out_channels = 16,
+                                           kernel_size = 5,#3,
                                            stride = 1)
         
-        decoder['out4'] = torch.nn.Sigmoid()
+        decoder['out5'] = PTanh(16)
+        
+        # sixth layer
+        
+        decoder['us6'] = torch.nn.Upsample(size = 3006,
+                                           mode = 'nearest')
+        
+        decoder['act1'] = PTanh(16)
+        
+        decoder['out6'] = torch.nn.Conv1d(in_channels = 16,
+                                          out_channels = 1,
+                                          kernel_size = 7,#3,
+                                          stride = 1)
         
         self.decoder = torch.nn.Sequential(decoder)
         
@@ -68,9 +133,9 @@ if __name__ == '__main__':
     
     x = torch.rand(8,1,3000)
     
-    # zero mean and unit variance
+    # zero mean and unit variance TODO: try 0.5 mean instead
 
-    x = torch.div((x - x.mean(dim = 2).unsqueeze(dim = -1)),
+    x = torch.div((x - x.mean(dim = 2).unsqueeze(dim = -1) + 0.5),
                    x.std(dim = 2).unsqueeze(dim = -1))
     
     """
@@ -78,6 +143,10 @@ if __name__ == '__main__':
     
     y = {x / (x.max - x.min)} - {x.min / (x.max - x.min)}
       = (1 / x.max - x.min) * (x - x.min)
+    
+    this is only necessary if the negative log-likelihood function for a
+    Bernoulli distribution (binary cross-entropy) will be used as a loss
+    function
     
     """
     
