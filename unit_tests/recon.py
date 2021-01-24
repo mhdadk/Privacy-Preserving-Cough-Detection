9505bdf1bd07391330674df9e2b7ba9ec7d56e37
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2021-01-24 19:14:21 +0400

    Update recon.py

--- unit_tests/recon.py
+++ unit_tests/recon.py
@@ -1,21 +1,25 @@
+import warnings
+warnings.simplefilter("ignore")
+
 import torch
+import torchaudio
 
 class PTanh(torch.nn.Module):
     
     # PTanh(x) = a * tanh(b * x)
     
-    def __init__(self,num_parameters):
+    def __init__(self,in_channels):
         
         super().__init__()
         
         # initialize a
         
-        a = torch.full((num_parameters,1), 1.7159)
+        a = torch.full((in_channels,1,1), 1.7159)
         self.a = torch.nn.Parameter(a,requires_grad = True)
         
         # initialize b
         
-        b = torch.full((num_parameters,1), 2/3)
+        b = torch.full((in_channels,1,1), 2/3)
         self.b = torch.nn.Parameter(b,requires_grad = True)
     
     def forward(self,x):
@@ -28,7 +32,7 @@ class PTanh(torch.nn.Module):
 batch_size = 8
 num_channels = 1
 length_sec = 1.5
-sample_rate = 2000
+sample_rate = 16000
 
 # to count the total number of parameters
 
@@ -36,12 +40,27 @@ op_params = []
 
 # example input
 
-x = torch.rand(batch_size,num_channels,int(length_sec * sample_rate))
+x1 = torch.rand(batch_size,num_channels,int(length_sec * sample_rate))
+print('Input shape: {}'.format(tuple(x1.shape)))
 
-# zero mean and unit variance
+# compute log Mel spectrogram
+        
+log = torchaudio.transforms.AmplitudeToDB()
+mel_spec = torchaudio.transforms.MelSpectrogram(sample_rate = sample_rate,
+                                                n_fft = 1024,
+                                                n_mels = 128,
+                                                hop_length = 64)
+x = log(mel_spec(x1))
+print('Log Mel-scale spectrogram shape: {}'.format(tuple(x.shape)))
 
-x = torch.div((x - x.mean(dim = 2).unsqueeze(dim = -1)),
-               x.std(dim = 2).unsqueeze(dim = -1))
+# zero mean and unit variance. The expression [(..., ) + (None, ) * 2] is
+# used to unsqueeze 2 dimensions at the end of x.mean(dim = (2,3))
+
+x = torch.div(x - x.mean(dim = (2,3))[(..., ) + (None, ) * 2],
+              x.std(dim = (2,3))[(..., ) + (None, ) * 2])
+print('Normalized shape: {}'.format(tuple(x.shape)))
+print('Normalized mean: {}'.format(x.mean()))
+print('Normalized variance: {}'.format(x.var()))
 
 """
 scale each signal to be between 0 and 1. This is equivalent to:
@@ -51,125 +70,131 @@ y = {x / (x.max - x.min)} - {x.min / (x.max - x.min)}
 
 """
 
-x = torch.multiply(torch.div(1,x.max(dim = 2)[0].unsqueeze(dim = -1) - 
-                               x.min(dim = 2)[0].unsqueeze(dim = -1)),
-                   x - x.min(dim = 2)[0].unsqueeze(dim = -1))
+# x = torch.multiply(torch.div(1,x.max(dim = 2)[0].unsqueeze(dim = -1) - 
+#                                x.min(dim = 2)[0].unsqueeze(dim = -1)),
+#                    x - x.min(dim = 2)[0].unsqueeze(dim = -1))
 
 #%%
 
 # encode
 
-conv1 = torch.nn.Conv1d(in_channels = 1,
+conv1 = torch.nn.Conv2d(in_channels = 1,
                         out_channels = 16,
                         kernel_size = 3,
                         stride = 1)
 
 op_params.append(conv1)
 
+y1 = conv1(x)
+
+print('conv1 shape: {}'.format(tuple(y1.shape)))
+
 act1 = PTanh(16)
 
-y1 = act1(conv1(x))
+y2 = act1(y1)
 
-pool1 = torch.nn.Conv1d(in_channels = 16,
+print('act1 shape: {}'.format(tuple(y2.shape)))
+
+pool1 = torch.nn.Conv2d(in_channels = 16,
                         out_channels = 16,
                         kernel_size = 2,
-                        stride = 2)
+                        stride = (1,2))
 
 op_params.append(pool1)
 
-y2 = pool1(y1)
+y3 = pool1(y2)
+
+print('pool1 shape: {}'.format(tuple(y3.shape)))
 
 #%%
 
-conv2 = torch.nn.Conv1d(in_channels = 16,#64,
-                        out_channels = 32,#56,
-                        kernel_size = 3,#5,#16,
+conv2 = torch.nn.Conv2d(in_channels = 16,
+                        out_channels = 32,
+                        kernel_size = 3,
                         stride = 1)
 
 op_params.append(conv2)
 
-y3 = activation(conv2(y2))
+y4 = conv2(y3)
 
-pool2 = torch.nn.Conv1d(in_channels = 32,
+print('conv2 shape: {}'.format(tuple(y4.shape)))
+
+act2 = PTanh(32)
+
+y5 = act2(y4)
+
+print('act2 shape: {}'.format(tuple(y5.shape)))
+
+pool2 = torch.nn.Conv2d(in_channels = 32,
                         out_channels = 32,
                         kernel_size = 2,
-                        stride = 2)
+                        stride = (1,2))
 
 op_params.append(pool2)
 
-y4 = pool2(y3)
+y6 = pool2(y5)
 
-#%%
-
-conv3 = torch.nn.Conv1d(in_channels = 32,#64,
-                        out_channels = 64,#56,
-                        kernel_size = 3,#7,#16,
-                        stride = 1)
-
-op_params.append(conv3)
-
-y5 = activation(conv3(y4))
-
-pool3 = torch.nn.Conv1d(in_channels = 64,
-                        out_channels = 64,
-                        kernel_size = 2,
-                        stride = 2)
-
-op_params.append(pool3)
-
-y6 = pool3(y5)
+print('pool2 shape: {}'.format(tuple(y6.shape)))
 
 #%%
 
 # decode
 
-us1 = torch.nn.Upsample(scale_factor = 2,
+us1 = torch.nn.Upsample(size = (130,130),
                         mode = 'nearest')
 
 y7 = us1(y6)
 
-conv4 = torch.nn.Conv1d(in_channels = 64,
-                        out_channels = 32,
-                        kernel_size = 3,#7,
+print('us1 shape: {}'.format(tuple(y7.shape)))
+
+conv3 = torch.nn.Conv2d(in_channels = 32,
+                        out_channels = 16,
+                        kernel_size = 3,
+                        stride = 1)
+
+op_params.append(conv3)
+
+y8 = conv3(y7)
+
+print('conv3 shape: {}'.format(tuple(y8.shape)))
+
+act3 = PTanh(16)
+
+y9 = act3(y8)
+
+print('act3 shape: {}'.format(tuple(y9.shape)))
+
+#%%
+
+us2 = torch.nn.Upsample(size = (152,162),
+                        mode = 'nearest')
+
+y10 = us2(y9)
+
+print('us2 shape: {}'.format(tuple(y10.shape)))
+
+conv4 = torch.nn.Conv2d(in_channels = 16,
+                        out_channels = 1,
+                        kernel_size = 3,
                         stride = 1)
 
 op_params.append(conv4)
 
-y8 = activation(conv4(y7))
+y11 = conv4(y10)
+
+print('conv4 shape: {}'.format(tuple(y11.shape)))
+
+act4 = PTanh(1)
+
+y12 = act4(y11)
+
+print('act4 shape: {}'.format(tuple(y12.shape)))
 
 #%%
 
-us2 = torch.nn.Upsample(scale_factor = 2,
-                        mode = 'nearest')
+# flatten to 1D signal
 
-y9 = us2(y8)
-
-conv5 = torch.nn.Conv1d(in_channels = 32,
-                        out_channels = 16,
-                        kernel_size = 3,#5,
-                        stride = 1)
-
-op_params.append(conv5)
-
-y10 = activation(conv5(y9))
-
-#%%
-
-us3 = torch.nn.Upsample(size = 3002,
-                        mode = 'nearest')
-
-y11 = us3(y10)
-
-conv6 = torch.nn.Conv1d(in_channels = 16,
-                        out_channels = 1,
-                        kernel_size = 3,#7,#3,
-                        stride = 1)
-
-op_params.append(conv6)
-
-y12 = activation(conv6(y11))
-
-#%%
+recon = torch.flatten(y12,start_dim=2)
 
 # compute total number of parameters
 
