ce84397abf037928b23d83b3d8c56da0dc7f59a4
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2021-02-03 22:57:29 +0400

    Update recon_test.py

--- recon_test.py
+++ recon_test.py
@@ -15,15 +15,25 @@ def run_batch(x,spec,net,mode,loss_func,optimizer,device):
     
     x = x.to(device)
     
+    x = spec(x)
+    
+    x = torchaudio.functional.magphase(x)[0]
+    
+    # scale each example in the batch to interval [0,1]
+    
+    scale_factor = x.amax(dim=(2,3))[(..., ) + (None, ) * 2]
+    
+    x_scaled = x / scale_factor
+    
     with torch.set_grad_enabled(mode == 'train'):
-        
-        x = spec(x)
-        
-        x = torchaudio.functional.magphase(x)[0]
         
         # compute reconstruction of input signal
         
-        x_hat = net(x)
+        x_hat = net(x_scaled)
+        
+        # re-scale back to normal values
+        
+        x_hat = x_hat * scale_factor
         
         # compute reconstruction loss
         
