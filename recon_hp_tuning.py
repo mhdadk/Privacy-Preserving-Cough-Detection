d8e0d49410e94d093d284c97d0fe0829f5a96c1e
Mahmoud Abdelkhalek <mahmoud.t7@gmail.com>
2021-02-04 15:12:17 +0400

    Update recon_hp_tuning.py

--- recon_hp_tuning.py
+++ recon_hp_tuning.py
@@ -261,4 +261,22 @@ if __name__ == '__main__':
                    n_jobs = 5,
                    gc_after_trial = True)
     
+    pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
+    complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
+    
     pickle.dump(study,open('results/study.pkl','wb'))
+    
